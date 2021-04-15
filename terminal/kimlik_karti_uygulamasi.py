49646f2e125b683794f5cb46ed8bc9cfbd4e1189
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-04-15 15:13:15 +0300

    Create kimlik_karti_uygulamasi.py

---
+++ terminal/kimlik_karti_uygulamasi.py
@@ -0,0 +1,19 @@
+
+isim = "Eren"
+soyisim = "İncesu"
+dogumm_tarihi = "2001"
+dogum_yeri = "İstanbul"
+print("------------------------------------")
+print("İsim            :",isim)
+print("Soyisim         :",soyisim)
+print("Dogum Yeri      :",dogum_yeri)
+print("Dogum Tarihi    :",dogumm_tarihi)
+print("------------------------------------")
+
+#Çıktı
+#------------------------------------
+#İsim            : Eren
+#Soyisim         : İncesu
+#Dogum Yeri      : İstanbul
+#Dogum Tarihi    : 2001
+#------------------------------------
