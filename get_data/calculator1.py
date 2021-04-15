73db9d3f0c49b3d83b48aede930182a751f099ad
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-04-15 15:22:51 +0300

    Create calculator1.py

---
+++ get_data/calculator1.py
@@ -0,0 +1,18 @@
+sayi1 = float(input("Birinci Sayıyı giriniz : "))
+sayi2 = float(input("İkinci Sayıyı giriniz  : "))
+
+print("\n")
+print("----------------------------")
+print("Toplama sonucu :",sayi1+sayi2)
+print("Çıkarma sonucu :",sayi1-sayi2)
+print("Çarpma sonucu  :",sayi1*sayi2)
+print("Bölüm sonucu   :",sayi1/sayi2)
+
+#Çıktı
+#Birinci Sayıyı giriniz : 4
+#İkinci Sayıyı giriniz  : 6
+#----------------------------
+#Toplama sonucu : 10.0
+#Çıkarma sonucu : -2.0
+#Çarpma sonucu  : 24.0
+#Bölüm sonucu   : 0.6666666666666666
