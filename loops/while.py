8cf3ff80bf97491376469bbea998641bc27b7ba1
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-04-15 15:35:24 +0300

    Create while.py

---
+++ loops/while.py
@@ -0,0 +1,23 @@
+x = 0
+
+while x <=10:
+    x += 1
+    print("Eren incesu adamdır")
+
+print(x)
+
+"""
+Çıktı
+Eren incesu adamdır
+Eren incesu adamdır
+Eren incesu adamdır
+Eren incesu adamdır
+Eren incesu adamdır
+Eren incesu adamdır
+Eren incesu adamdır
+Eren incesu adamdır
+Eren incesu adamdır
+Eren incesu adamdır
+Eren incesu adamdır
+11
+"""
