31895bb6765750bc871e00de7f3ce3bf16cb6ab2
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-04-15 15:42:19 +0300

    Create operators.py

---
+++ operators/operators.py
@@ -0,0 +1,35 @@
+"""
+Boolean operatörler
+
+and == ve
+or == veya 
+not == değil
+
+"""
+
+"""
+Aritmetik operatörler 
+
++ == toplama
+- == çıkarma
+* == çarpma 
+/ == bölme
+** == kuvvet
+// == taban bölme
+% == mod alma
+"""
+
+"""
+== == eşit
+!= == eşit değil
+> == büyüktür
+< == küçüktür
+>= == büyük eşit
+<= == küçük eşit
+"""
+
+"""
+Aitlik operatörü
+
+in == içinde
+"""
