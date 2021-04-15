77682285914f34e369e7a2702e0d32b1b287056b
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-04-15 15:29:30 +0300

    Create if_elif_else.py

---
+++ control_mechanisms/if_elif_else.py
@@ -0,0 +1,22 @@
+"""
+   == --->>> eşit ise
+
+
+
+"""
+x = input("Sayı Giriniz: ")
+
+x = int(x)
+
+if bool(x):
+    print("İçerisi Dolu")
+
+else:
+    print("İçerisi boş")
+
+"""
+Çıktı
+Sayı Giriniz : 5 girdiğimizi varsayalım bu noktada if bloğu çalışacak ve 
+İçeri Dolu yazdıracak
+"""
+
