29d785e6b0105c97d4a95ae15fd88407de028273
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-04-15 15:34:10 +0300

    Create for_example.py

---
+++ loops/for_example.py
@@ -0,0 +1,16 @@
+for sayi in range(0,100000,10000):
+    print("Bu",str(sayi) + "'ıncı döngü")
+
+"""
+Çıktı
+Bu 0'ıncı döngü
+Bu 10000'ıncı döngü
+Bu 20000'ıncı döngü
+Bu 30000'ıncı döngü
+Bu 40000'ıncı döngü
+Bu 50000'ıncı döngü
+Bu 60000'ıncı döngü
+Bu 70000'ıncı döngü
+Bu 80000'ıncı döngü
+Bu 90000'ıncı döngü
+"""
