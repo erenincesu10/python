e50ecc3135a81586cc684c86a98efb9f71800e37
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-10 00:50:42 +0300

    Create string.py

---
+++ btk/objectsandDataStructers/string.py
@@ -0,0 +1,13 @@
+
+
+#string tipinde veri tanımlama
+string = "Hello World"
+
+#string ifadenin bir indexindeki elemanını yazdırma
+print(string[1])
+
+#string dizisinin karakter sayısını bulma
+print(len(string))
+
+#bir indexten diğerine kadar yazdırma
+print(string[2:5])
