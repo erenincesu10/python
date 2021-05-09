4dfa5d7b7de2939ca078d7dce16e0fc2535dc799
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-10 01:18:16 +0300

    Create stringExamples.py

---
+++ btk/objectsandDataStructers/stringExamples.py
@@ -0,0 +1,47 @@
+
+website = "https://www.erenincesu.com"
+course =  "Python Kursu : Baştan Sona Python Programlama Rehberiniz (40 saat)"
+
+""" 1- 'course' karakter dizisinde kaç karakter bulunmaktadır ?
+uzunluk = len(course)
+print(uzunluk)
+"""
+
+"""2- 'website' içinden www karakterlerini alın.
+result = website[8:11]
+print(result)
+"""
+
+"""3- 'website' içinden com karakterlerini alın.
+result = website[uzunluk-3:uzunluk]
+print(result)
+"""
+
+"""4- 'course' içinden ilk 15 ve son 15 karakterlerini alın
+ilkOnBes = course[:15]
+sonOnBes = course[-15:]
+print(ilkOnBes,sonOnBes)
+"""
+
+"""5- 'course' içerisindeki tersten yazdırın
+result = course[::-1]
+print(result)
+"""
+
+"""name, surname, age, job = "Eren", "Kral", 25, "Muhendis"
+  6- Yukarıda verilen değişkenler ile ekrana aşağıdaki ifadeyi yazdırın.
+    'Benim adım Eren Kral, Yaşım 25 ve mesleğim Muhendis'
+print("Benim adım {} {}, Yaşım {} ve mesleğim {}".format(name,surname,age,job))
+"""
+
+"""7- 'Hello world' ifadesindeki w harfini 'W' harfi ile değiştirin.
+kelime = "Hello world"
+kelime = kelime[0:6] + "W" + kelime[-4:]
+print(kelime)
+"""
+
+"""8- 'abc' ifadesini yan yana 3 defa yazdırın
+kelime = 'abc'
+result = 'abc' * 3
+print(result)
+"""
