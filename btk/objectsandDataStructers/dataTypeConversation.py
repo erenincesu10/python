1b7a31ef72f2ceed0b04afe7a8be174d42045772
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-10 00:42:10 +0300

    Create dataTypeConversation.py

---
+++ btk/objectsandDataStructers/dataTypeConversation.py
@@ -0,0 +1,21 @@
+"""
+#inputtan gelen string verileri int tipine çevirdik
+x = int(input("Bir sayi giriniz : "))
+y = int(input("İkinci sayiyi giriniz : "))
+
+toplam = x + y
+"""
+
+x = 4
+y = 1.5
+name = "Eren"
+isOnline = True
+
+#int to float
+x = float(x)
+
+#float to int
+y = int(y)
+
+#bool to str
+isOnline = str(isOnline)
