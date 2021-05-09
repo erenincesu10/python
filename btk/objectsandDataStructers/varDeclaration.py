a402017d7bc4b998ecab8866f29daf760644b16f
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-10 00:32:12 +0300

    Create varDeclaration.py

---
+++ btk/objectsandDataStructers/varDeclaration.py
@@ -0,0 +1,14 @@
+
+
+#int tipinde değişken tanımlama
+numara = 9
+
+#float tipinde değişken tanımlama
+ortalama = 75.5
+
+#string tipinde değişken tanımlama
+isim = "Ero"
+
+#boolean tipinde değişken tanımlama
+dogruMu = True
+
