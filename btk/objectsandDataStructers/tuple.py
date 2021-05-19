2483e1af36733daac09aa36baac82dcf3a60fe5f
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-20 01:07:52 +0300

    Create tuple.py

---
+++ btk/objectsandDataStructers/tuple.py
@@ -0,0 +1,13 @@
+
+
+tuple = (1,"iki",3)
+print(type(tuple))
+
+print(tuple[2])
+
+print(len(tuple))
+
+#atandıktan sonra tuple[0] = "ahmet" gibi bir değişiklik yapamayız.
+
+print(tuple.count(1))
+print(tuple.index("iki"))
