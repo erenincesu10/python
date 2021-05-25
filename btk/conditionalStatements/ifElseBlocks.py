76193147c4fdd8220569f8b3c0bfa84f276adcc6
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-25 18:48:10 +0300

    Create ifElseBlocks.py

---
+++ btk/conditionalStatements/ifElseBlocks.py
@@ -0,0 +1,15 @@
+
+
+username = "erenincesu"
+password = "123456"
+
+userName = input("Username giriniz :")
+Password = input("Password giriniz : ")
+
+if username == userName:
+    if password == Password:
+        print("Hoş geldiniz!")
+    else:
+        print("Parolanizi yanlis girdiniz")
+else:
+    print("Username yanlis!")
