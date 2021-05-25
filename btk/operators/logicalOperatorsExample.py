c9e76a51bb875981a13f14c252d5331d498c8db1
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-25 18:36:39 +0300

    Create logicalOperatorsExample.py

---
+++ btk/operators/logicalOperatorsExample.py
@@ -0,0 +1,22 @@
+
+"""
+1- Girilen bir sayının 0-100 arasında olup olmadığını kontrol ediniz.
+
+sayi = int(input("Bir sayı giriniz : "))
+
+if (sayi > 0) and (sayi < 100):
+    print("True")
+else:
+    print("False")
+"""
+
+"""
+2- Girilen bir sayının pozitif çift sayı olup olmadığını kontrol ediniz.
+sayi = int(input("Bir sayi giriniz :"))
+
+if sayi >= 0 and sayi%2 == 0:
+    print("True")
+else:
+    print("False")
+"""
+
