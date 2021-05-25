fce2f52727007c65aae7324cf24b50549a371993
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-25 14:31:33 +0300

    Create comparisonOperators.py

---
+++ btk/operators/comparisonOperators.py
@@ -0,0 +1,35 @@
+
+
+
+#username , password => database
+
+# 'erenincesu' , '123456'
+
+a, b, c, d = 5, 5, 10, 4
+
+password = "123456"
+username = "erenincesu"
+
+result = (a == b) #True döndürür
+print(result)
+
+result = (a == c) #False döndürür.eşit mi operatörü
+print(result)
+
+result = (a != c) #True döndürür.eşit değil mi operatörü
+print(result)
+
+result = ("erenncs" == username)
+print(result)
+
+result = (a > b)
+print(result)
+
+result = (a > c)
+print(result)
+
+result = (a >= b)
+print(result)
+
+result = (True == 1)
+print(result)
