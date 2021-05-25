647405d3d5e1d8fd8698085c5ef46c5868d0a78b
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-25 18:40:47 +0300

    Create identityandMembershipOperators.py

---
+++ btk/operators/identityandMembershipOperators.py
@@ -0,0 +1,22 @@
+
+
+# Identity Operator : is
+
+x = y = [1, 2, 3]
+z = [1, 2, 3]
+
+print(x==y)
+print(x==z)
+print(x is y)
+print(x is z)
+print(x is not z)
+
+# Membership Operator : in
+
+print(1 in x)
+print(4 in y)
+
+name = "Eren"
+print("E" in name)
+  
+  
