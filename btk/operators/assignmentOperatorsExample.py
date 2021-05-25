f2e445b6b795a9aa97eeb4dd91be145d32e113ea
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-25 14:25:56 +0300

    Create assignmentOperatorsExample.py

---
+++ btk/operators/assignmentOperatorsExample.py
@@ -0,0 +1,43 @@
+
+x, y, z = 2, 5, 10
+
+numbers = 1, 5, 7, 10, 6
+
+"""
+1- kullanıcıdan aldığınız 2 sayının çarpımı ile x, y, z toplamının farkı nedir ?
+firstN = int(input("Birinci sayiyi giriniz : "))
+secondN = int(input("İkinci sayiyi giriniz : "))
+
+print((firstN*secondN)-(x+y+z))
+"""
+
+"""
+2- y'nin x'e kalansız bölümünü hesaplayınız.
+sonuc = y//x
+print(sonuc)
+"""
+
+"""
+3- (x,y,z) toplamının mod 3'ü nedir ?
+sonuc = (x+y+z)%3
+print(sonuc)
+"""
+
+"""
+4- y'nin x. kuvvetini hesaplayınız.
+sonuc = y**x
+print(sonuc)
+"""
+
+"""
+5- x, *y, z = numbers işlemine göre z'nin küpü kaçtır ?
+x, *y, z = numbers
+print((z**3))
+"""
+
+"""
+6- x, *y, z = numbers işlemine göre y'nin değerleri toplamı kaçtır ?
+x , *y, z = numbers
+toplam = y[0] + y[1] + y[2]
+print(toplam)
+"""
