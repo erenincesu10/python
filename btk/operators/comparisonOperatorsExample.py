69ecfd3f94aa5de2e759496507b92e8352d658a4
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-25 18:25:48 +0300

    Create comparisonOperatorsExample.py

---
+++ btk/operators/comparisonOperatorsExample.py
@@ -0,0 +1,46 @@
+
+"""
+1- Girilen 2 sayıdan hangisi büyüktür ?
+
+a = int(input("a : "))
+b = int(input("b : "))
+
+result = (a > b)
+print(f"a: {a} b: {b}'den büyüktür {result}")
+"""
+
+"""
+2- Kullanıcıdan vize (%60) ve final (%40) notunu alıp ortalama hesaplayınız.
+   Eğer ortalama 50 ve üstündeyse geçti değilse kaldı yazdırın.
+
+vize = int(input("Vize notunu giriniz : "))
+final = int(input("Final notunu giriniz : "))
+
+ortalama = (vize*0.6 + final*0.4)
+print(ortalama)
+
+if ortalama >= 50:
+    print("Tebrikler sınıfı geçtiniz!")
+else:
+    print("Maalesef geçemediniz!")
+"""
+
+"""
+3- girilen bir sayının tek mi çift olduğunu yazdırın.
+
+sayi = int(input("Bir sayi giriniz : "))
+if sayi%2 == 0:
+    print("Çift sayi girdiniz!")
+else:
+    print("Tek sayi girdiniz!")
+"""
+
+"""
+4- Girilen bir sayının negatif pozitif durumunu yazdırın.
+
+sayi = int(input("Bir sayi giriniz : "))
+if sayi >= 0:
+    print("Pozitif bir sayi girdiniz!")
+else:
+    print("Negatif bir sayi girdiniz!")
+"""
