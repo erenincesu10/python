7d5aa5aa2fd13937ecb7e5c4e5d9d3c85145e44c
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-28 12:12:45 +0300

    Create collatzArray.py

---
+++ algorithms/collatzArray.py
@@ -0,0 +1,19 @@
+
+enUzun = 0
+aranan = 0
+
+for sayi in range(999999, 300000, -1):
+    zincir = 0
+    n = sayi
+    while n != 1:
+        zincir += 1
+        if n%2 == 0:
+            n //=  2 #// bölümün tam sayılı kısmını alır
+        else:
+            n = 3*n + 1
+
+    if zincir > enUzun:
+        enUzun = zincir
+        aranan = sayi
+
+print("Zincir sayısı : ",enUzun,"Sayımız : ",aranan)
