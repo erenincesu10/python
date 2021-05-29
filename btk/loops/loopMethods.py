642bfaf68eef67a0f330673b1c0098b36438f5d0
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-30 01:36:08 +0300

    Create loopMethods.py

---
+++ btk/loops/loopMethods.py
@@ -0,0 +1,29 @@
+
+"""
+range metotları
+
+for item in range(2,10):
+    print(item)
+
+print(list(range(5,100,20)))
+"""
+
+"""
+#enumerate numaralandırarak listeler
+greeting = "Hello there"
+
+for index,letter in enumerate(greeting):
+    print(f"index : {index} letter : {greeting[index]}")
+    index += 1
+"""
+
+"""
+# zip eşleştirme yapmamızı sağlar
+list1 = [1,2,3,4,5]
+list2 = ["a","b","c","d","e"]
+
+print(list(zip(list1,list2)))
+
+for a,b in zip(list1,list2):
+    print(a,b)
+"""
