c3a55da29d4f950c7c902613428081a03d9ae048
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-05-30 00:22:11 +0300

    Create for.py

---
+++ btk/loops/for.py
@@ -0,0 +1,17 @@
+
+number = [1,2,3,4,5]
+
+for i in number:
+    print(i)
+
+
+tuple = (1,2,3,4,5)
+
+for i in tuple:
+    print(i)
+
+d = {"k1":1,"k2":2,"k3":3}
+
+for key,value in d.items(): #valueları yazdırır
+    print(key,value)
+
