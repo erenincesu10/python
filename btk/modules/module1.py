6c661cf9af124ffe0126acc7cc6340e3853eb416
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-06-25 22:06:53 +0300

    Create module1.py

---
+++ btk/modules/module1.py
@@ -0,0 +1,19 @@
+
+#import math
+#import math as m
+#from math import *
+
+def sqrt(x):
+    print("x : "+ str(x)) #hangi fonksiyon sonra eklendiyse o fonksiyonu alır!
+
+from math import factorial,sqrt
+
+#value = dir(math)
+#value = help(math)
+value = sqrt(49)
+value = factorial(5)
+value = floor(5.9) #floor ve ceil import etmediğimiz için fonksiyonu tanımlayamadı!
+value = ceil(5.9)
+
+print(value)
+
