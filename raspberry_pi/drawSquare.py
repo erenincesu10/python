f83990b767e89b77fd7ae92c983f5af5e4ff47b8
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2021-07-07 13:53:56 +0300

    Create drawSquare.py

---
+++ raspberry_pi/drawSquare.py
@@ -0,0 +1,11 @@
+
+from turtle import *
+
+shape('turtle')  # this code means turns the arrow to a turtle
+
+i = 0
+
+while i < 4:
+  forward(100) #forward(n) || fd(n) => n units move forward
+  left(90)     #left(n) ||lt(n) => n  turns n degrees to the left
+  i += 1
