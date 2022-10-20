377112a14502f9a8474c9475dba103bf906a3685
Eren İncesu <58305952+erenincesu10@users.noreply.github.com>
2022-10-20 11:59:03 +0300

    Update math_operators.py

--- hackerrank/basics/math_operators.py
+++ hackerrank/basics/math_operators.py
@@ -1,9 +1,15 @@
     a = int(input())
     b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
     print(a//b)
     print(a/b)
     
     #result a = 6, b = 8
+    #14
+    #-2
+    #48
     #0
     #0.6666666666666666
     
